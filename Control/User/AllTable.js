3118373dc14efc84e31180f3ebe6e352e115728b
LoudAsh21 <thiagosancho2006@gmail.com>
2024-06-02 10:34:14 -0300

    fix user datatable

--- Control/User/AllTable.js
+++ Control/User/AllTable.js
@@ -2,13 +2,16 @@ const ModelUser = require("../../Model/User");
 
 module.exports = async (request, response) => {
   const user = new ModelUser();
-  const fields = ["register", "name", "email", "phone_number", "user_type"];
+  const fields = ["register", "name", "email", "phone_number", "user_type" , "course_id"];
 
   try{
     const data = await user.allFields(fields);
     const format = data.map((user)=>({
       _id: user._id,
+      register: user.register,
+      course_name: user.course_id.name,
       name: user.name,
+      
       email: user.email,
       phone_number: user.phone_number ? user.phone_number : null,
       user_type: user.user_type
